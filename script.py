bda52150f4fbb87baa079d880a266f461fe63d80
Jonah Ezekiel <jonahezekiel21@gmail.com>
2023-06-05 17:04:08 -0700

    yes

--- script.py
+++ script.py
@@ -4,22 +4,6 @@ import pandas as pd
 import random
 import matplotlib.pyplot as plt
 
-class PolicyAgent:
-    def __init__(self, target_allocation):
-        self.target_allocation = target_allocation
-        self.action = None
-
-    def get_action(self, money, stock, price):
-        if stock * price + money == 0:  # to prevent division by zero
-            return 'Hold'
-        current_allocation = (stock * price) / (money + stock * price)
-        if current_allocation < self.target_allocation:
-            return 'Buy'
-        elif current_allocation > self.target_allocation:
-            return 'Sell'
-        else:
-            return 'Hold'
-
 class Agent:
     def __init__(self, lr, gamma, e_start, e_end, e_decay, state_space):
         self.state = None
@@ -67,21 +51,56 @@ class Agent:
         self.action = action
 
 
+class SarsaAgent(Agent):
+    def update_q_value(self, reward, next_state, next_action):
+        next_state = self.discretize(next_state)
+        next_q_value = self.get_q_value(next_state, next_action)
+        if (self.state, self.action) not in self.q_table:
+            self.q_table[(self.state, self.action)] = 0
+        self.q_table[(self.state, self.action)] += self.lr * (reward + self.gamma * next_q_value - self.get_q_value(self.state, self.action))
+
+
+class ValueIterationAgent(Agent):
+    def __init__(self, lr, gamma, state_space):
+        super().__init__(lr, gamma, None, None, None, state_space)
+        self.v_table = {}
+
+    def init_v(self, state):
+        state = self.discretize(state)
+        if state not in self.v_table:
+            self.v_table[state] = 0
+
+    def get_action(self, state):
+        state = self.discretize(state)
+        q_values = [self.get_q_value(state, a) for a in ['Buy', 'Sell', 'Hold']]
+        action = ['Buy', 'Sell', 'Hold'][np.argmax(q_values)]
+        return action
+
+    def update_q_value(self, state, action, reward, next_state):
+        next_state = self.discretize(next_state)
+        # We don't need to calculate q_value_next here, because it's calculated in the update_v_value function.
+        if (state, action) not in self.q_table:
+            self.q_table[(state, action)] = 0
+        self.q_table[(state, action)] = reward + self.gamma * self.v_table[next_state]
+    
+    def update_v_value(self, state):
+        state = self.discretize(state)
+        v_value_next = max([self.get_q_value(state, a) for a in ['Buy', 'Sell', 'Hold']])
+        self.v_table[state] = v_value_next
+
+
 def simulate_trading(agent, data, money=10000, stock=0, agent_type='Q'):
     state = data[0]
-    if agent_type == 'Q':
+    if agent_type == 'Value':
+        agent.init_v(state)
+    else:
         state = agent.discretize(state)
         agent.update_state_action(state, 'Hold')
-        action = agent.get_action(state)
-    elif agent_type == 'Policy':
-        action = agent.get_action(money, stock, state)
+
     portfolio_values = []
     for i in range(len(data)):
-        if agent_type == 'Q':
-            state = agent.discretize(state)
-            action = agent.get_action(state)
-        else:
-            action = agent.get_action(money, stock, state)
+        state = agent.discretize(state)
+        action = agent.get_action(state)
 
         if action == 'Buy' and money >= state:
             stock += 1
@@ -103,9 +122,22 @@ def simulate_trading(agent, data, money=10000, stock=0, agent_type='Q'):
             agent.update_q_value(reward, next_state)
             agent.update_state_action(state, action)
             agent.update_epsilon()
+        elif agent_type == 'Sarsa':
+            next_action = agent.get_action(next_state)
+            agent.update_q_value(reward, next_state, next_action)
+            agent.update_state_action(state, action)
+            agent.update_epsilon()
+        elif agent_type == 'Value':
+            agent.update_q_value(state, action, reward, next_state)
+            agent.update_v_value(next_state)
+            agent.update_state_action(state, action)
 
     return portfolio_values
 
+# Create and train the agents
+q_agent = Agent(lr=0.01, gamma=0.95, e_start=1.0, e_end=0.01, e_decay=0.995, state_space=100)
+sarsa_agent = SarsaAgent(lr=0.01, gamma=0.95, e_start=1.0, e_end=0.01, e_decay=0.995, state_space=100)
+value_iteration_agent = ValueIterationAgent(lr=0.01, gamma=0.95, state_space=100)
 
 # Download SPY stock data
 raw_data = yf.download('SPY','2000-01-01','2023-12-31')['Close']
@@ -117,49 +149,25 @@ data = (raw_data - raw_data.min()) / (raw_data.max() - raw_data.min())
 train_data = data['2000-01-01':'2014-12-31']
 test_data = data['2015-01-01':'2023-12-31']
 
-# Create and train the agent
-q_agent = Agent(lr=0.01, gamma=0.95, e_start=1.0, e_end=0.01, e_decay=0.995, state_space=100)
-policy_agent = PolicyAgent(target_allocation=0.6)  # adjust the target allocation as needed
-
 # Train and test the Q-Learning agent
 q_train_portfolio_values = simulate_trading(q_agent, train_data, agent_type='Q')
 q_test_portfolio_values = simulate_trading(q_agent, test_data, agent_type='Q')
 
-# Train and test the policy-based agent
-policy_train_portfolio_values = simulate_trading(policy_agent, train_data, agent_type='Policy')
-policy_test_portfolio_values = simulate_trading(policy_agent, test_data, agent_type='Policy')
+# Train and test the Sarsa agent
+sarsa_train_portfolio_values = simulate_trading(sarsa_agent, train_data, agent_type='Sarsa')
+sarsa_test_portfolio_values = simulate_trading(sarsa_agent, test_data, agent_type='Sarsa')
 
-# Append the initial portfolio value to the beginning of portfolio values
-q_train_portfolio_values = [10000] + q_train_portfolio_values
-q_test_portfolio_values = [10000] + q_test_portfolio_values
-policy_train_portfolio_values = [10000] + policy_train_portfolio_values
-policy_test_portfolio_values = [10000] + policy_test_portfolio_values
+# Train and test the Value Iteration agent
+value_iteration_train_portfolio_values = simulate_trading(value_iteration_agent, train_data, agent_type='Value')
+value_iteration_test_portfolio_values = simulate_trading(value_iteration_agent, test_data, agent_type='Value')
 
-# Create new date indices for the portfolio values
-train_portfolio_index = pd.date_range(start=train_data.index[0], periods=len(q_train_portfolio_values))
-test_portfolio_index = pd.date_range(start=test_data.index[0], periods=len(q_test_portfolio_values))
-
-# Convert to DataFrame for easier manipulation
-df_q_train_portfolio = pd.DataFrame(q_train_portfolio_values, index=train_portfolio_index, columns=['Q-Agent Portfolio'])
-df_q_test_portfolio = pd.DataFrame(q_test_portfolio_values, index=test_portfolio_index, columns=['Q-Agent Portfolio'])
-df_policy_train_portfolio = pd.DataFrame(policy_train_portfolio_values, index=train_portfolio_index, columns=['Policy-Agent Portfolio'])
-df_policy_test_portfolio = pd.DataFrame(policy_test_portfolio_values, index=test_portfolio_index, columns=['Policy-Agent Portfolio'])
-
-# Normalize portfolio values
-df_q_train_portfolio['Q-Agent Portfolio'] = (df_q_train_portfolio['Q-Agent Portfolio'] - df_q_train_portfolio['Q-Agent Portfolio'].min()) / (df_q_train_portfolio['Q-Agent Portfolio'].max() - df_q_train_portfolio['Q-Agent Portfolio'].min())
-df_q_test_portfolio['Q-Agent Portfolio'] = (df_q_test_portfolio['Q-Agent Portfolio'] - df_q_test_portfolio['Q-Agent Portfolio'].min()) / (df_q_test_portfolio['Q-Agent Portfolio'].max() - df_q_test_portfolio['Q-Agent Portfolio'].min())
-df_policy_train_portfolio['Policy-Agent Portfolio'] = (df_policy_train_portfolio['Policy-Agent Portfolio'] - df_policy_train_portfolio['Policy-Agent Portfolio'].min()) / (df_policy_train_portfolio['Policy-Agent Portfolio'].max() - df_policy_train_portfolio['Policy-Agent Portfolio'].min())
-df_policy_test_portfolio['Policy-Agent Portfolio'] = (df_policy_test_portfolio['Policy-Agent Portfolio'] - df_policy_test_portfolio['Policy-Agent Portfolio'].min()) / (df_policy_test_portfolio['Policy-Agent Portfolio'].max() - df_policy_test_portfolio['Policy-Agent Portfolio'].min())
-
-# Plot the training and testing portfolio values over time
-plt.figure(figsize=(12,6))
-plt.plot(raw_data.index, data, label='S&P 500', color='grey')
-plt.plot(df_q_train_portfolio.index, df_q_train_portfolio['Q-Agent Portfolio'], label='Q-Agent Training Portfolio')
-plt.plot(df_q_test_portfolio.index, df_q_test_portfolio['Q-Agent Portfolio'], label='Q-Agent Testing Portfolio')
-plt.plot(df_policy_train_portfolio.index, df_policy_train_portfolio['Policy-Agent Portfolio'], label='Policy-Agent Training Portfolio')
-plt.plot(df_policy_test_portfolio.index, df_policy_test_portfolio['Policy-Agent Portfolio'], label='Policy-Agent Testing Portfolio')
-plt.title('Normalized Portfolio Value and S&P 500 Over Time')
-plt.xlabel('Date')
-plt.ylabel('Normalized Value')
+# Plot the results
+plt.figure(figsize=(12, 9))
+plt.plot(q_test_portfolio_values, color='blue', label='Q-Learning Agent')
+plt.plot(sarsa_test_portfolio_values, color='red', label='Sarsa Agent')
+plt.plot(value_iteration_test_portfolio_values, color='green', label='Value Iteration Agent')
+plt.title('Performance of the Q-Learning, Sarsa and Value Iteration Agents')
+plt.xlabel('Days')
+plt.ylabel('Portfolio Value')
 plt.legend()
 plt.show()
