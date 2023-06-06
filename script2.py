cfd40c13aa1b4f60d892dabf8f2d3c73ef0aa414
Jonah Ezekiel <jonahezekiel21@gmail.com>
2023-06-05 18:03:24 -0700

    not compiled code

--- script2.py
+++ script2.py
@@ -11,9 +11,6 @@ data['daily_return'] = data['Close'].pct_change()
 training_data = data.loc['2000-01-01':'2015-12-31']
 testing_data = data.loc['2016-01-01':]
 
-# Here the action space is simplified to only {0, 1} - not investing or investing all-in.
-# State space is the past N days returns.
-
 N = 5
 actions = [0, 1]
 epsilon = 0.1
@@ -59,11 +56,43 @@ class QLearning:
         q_target = r + self.gamma * self.q[str(s_)][a_]
         self.q[str(s)][a] += self.alpha * (q_target - q_predict)
 
+class ValueIteration:
+    def __init__(self, actions, epsilon, alpha, gamma, theta=0.0001):
+        self.v = defaultdict(lambda: 0.)
+        self.q = defaultdict(lambda: defaultdict(lambda: 0.))
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        self.theta = theta
+        self.actions = actions
+
+    def choose_action(self, state):
+        if np.random.uniform() < self.epsilon:
+            return np.random.choice(self.actions)
+        else:
+            return max(self.actions, key=lambda x: self.q[state][x])
+
+    def learn(self, s, a, r, s_):
+        old_v = 0
+        while True:
+            delta = 0
+            for s in states:
+                if s == 'terminal':
+                    continue
+                action = self.choose_action(s)
+                self.q[s][action] = r + self.gamma * self.v[s_]
+                v = max(self.q[s][action] for action in self.actions)
+                delta = max(delta, abs(v - self.v[s]))
+                self.v[s] = v
+            if delta < self.theta:
+                break
+
 # Training
 q_learning = QLearning(actions, epsilon, alpha, gamma)
 sarsa = SARSA(actions, epsilon, alpha, gamma)
+value_iteration = ValueIteration(actions, epsilon, alpha, gamma)
 
-agents = [q_learning, sarsa]
+agents = [q_learning, sarsa, value_iteration]
 
 states = [str(training_data['daily_return'].values[i-N:i]) for i in range(N, len(training_data['daily_return']))]
 for i in range(N, len(training_data['daily_return'])):
@@ -77,12 +106,14 @@ for i in range(N, len(training_data['daily_return'])):
         elif agent == sarsa:
             next_action = agent.choose_action(next_state)
             agent.learn(state, action, reward, next_state, next_action)
+        elif agent == value_iteration:
+            agent.learn(state, action, reward, next_state)
 
 # Testing and plotting
 test_states = [str(testing_data['daily_return'].values[i-N:i]) for i in range(N, len(testing_data['daily_return']))]
-portfolios = {'Q-Learning': [100.], 'SARSA': [100.]}
+portfolios = {'Q-Learning': [100.], 'SARSA': [100.], 'Value Iteration': [100.]}
 
-for agent, portfolio in zip([q_learning, sarsa], portfolios.values()):
+for agent, portfolio in zip([q_learning, sarsa, value_iteration], portfolios.values()):
     for i in range(N, len(testing_data['daily_return'])):
         state = test_states[i-N]
         action = agent.choose_action(state)
